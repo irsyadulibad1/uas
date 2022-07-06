92fd04ba4aba868a54fa0d23ea3a175a6472deeb
irsyadulibad1 <108770715+irsyadulibad1@users.noreply.github.com>
2022-07-06 07:40:28 +0700

    Add files via upload

---
+++ 2019/data/nakes_1.js
@@ -0,0 +1,26 @@
+var json_nakes_1 = {
+"type": "FeatureCollection",
+"name": "nakes_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Dokter": "5", "Perawat": "28", "Bidan": "51" }, "geometry": { "type": "Point", "coordinates": [ 110.919427556339912, -6.897311246137728 ] } },
+{ "type": "Feature", "properties": { "Dokter": "2", "Perawat": "19", "Bidan": "35" }, "geometry": { "type": "Point", "coordinates": [ 110.984590086925564, -6.885147573761741 ] } },
+{ "type": "Feature", "properties": { "Dokter": "2", "Perawat": "12", "Bidan": "26" }, "geometry": { "type": "Point", "coordinates": [ 111.040195446358638, -6.883409906279457 ] } },
+{ "type": "Feature", "properties": { "Dokter": "4", "Perawat": "29", "Bidan": "47" }, "geometry": { "type": "Point", "coordinates": [ 111.098407307015151, -6.826935713105232 ] } },
+{ "type": "Feature", "properties": { "Dokter": "5", "Perawat": "25", "Bidan": "34" }, "geometry": { "type": "Point", "coordinates": [ 111.04627728254664, -6.802608368353257 ] } },
+{ "type": "Feature", "properties": { "Dokter": "7", "Perawat": "15", "Bidan": "40" }, "geometry": { "type": "Point", "coordinates": [ 111.165307505083078, -6.824329211881806 ] } },
+{ "type": "Feature", "properties": { "Dokter": "4", "Perawat": "15", "Bidan": "29" }, "geometry": { "type": "Point", "coordinates": [ 111.216568695810452, -6.781756358565851 ] } },
+{ "type": "Feature", "properties": { "Dokter": "2", "Perawat": "20", "Bidan": "32" }, "geometry": { "type": "Point", "coordinates": [ 111.113177480614567, -6.751347177625884 ] } },
+{ "type": "Feature", "properties": { "Dokter": "4", "Perawat": "19", "Bidan": "48" }, "geometry": { "type": "Point", "coordinates": [ 111.146193162777962, -6.69139764948709 ] } },
+{ "type": "Feature", "properties": { "Dokter": "6", "Perawat": "22", "Bidan": "37" }, "geometry": { "type": "Point", "coordinates": [ 111.083637133415735, -6.696610651933942 ] } },
+{ "type": "Feature", "properties": { "Dokter": "4", "Perawat": "27", "Bidan": "52" }, "geometry": { "type": "Point", "coordinates": [ 111.043670781323215, -6.730495167838478 ] } },
+{ "type": "Feature", "properties": { "Dokter": "4", "Perawat": "11", "Bidan": "34" }, "geometry": { "type": "Point", "coordinates": [ 111.001097928007255, -6.7496095101436 ] } },
+{ "type": "Feature", "properties": { "Dokter": "2", "Perawat": "8", "Bidan": "29" }, "geometry": { "type": "Point", "coordinates": [ 111.014130434124382, -6.674020974664252 ] } },
+{ "type": "Feature", "properties": { "Dokter": "3", "Perawat": "13", "Bidan": "22" }, "geometry": { "type": "Point", "coordinates": [ 110.948967903538744, -6.6688079722174 ] } },
+{ "type": "Feature", "properties": { "Dokter": "6", "Perawat": "16", "Bidan": "49" }, "geometry": { "type": "Point", "coordinates": [ 111.072342294780896, -6.59669477170262 ] } },
+{ "type": "Feature", "properties": { "Dokter": "4", "Perawat": "21", "Bidan": "45" }, "geometry": { "type": "Point", "coordinates": [ 111.056703287440342, -6.556728419610091 ] } },
+{ "type": "Feature", "properties": { "Dokter": "2", "Perawat": "10", "Bidan": "15" }, "geometry": { "type": "Point", "coordinates": [ 110.989803089372415, -6.562810255798085 ] } },
+{ "type": "Feature", "properties": { "Dokter": "3", "Perawat": "13", "Bidan": "25" }, "geometry": { "type": "Point", "coordinates": [ 111.014999267865534, -6.47940221664846 ] } },
+{ "type": "Feature", "properties": { "Dokter": "3", "Perawat": "17", "Bidan": "30" }, "geometry": { "type": "Point", "coordinates": [ 110.962000409655872, -6.516762067517562 ] } }
+]
+}
